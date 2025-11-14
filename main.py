d580e10ad707de54316d8b30dfc2f09851d48404
djana <djanamali974@gmail.com>
2025-11-14 14:31:43 +0100

    model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from data.load_data import load_dataset
-from models.train_model import train_model
+from model.train_model import train_model
 
 def main():
     print("=== Pipeline Machine Learning ===")
